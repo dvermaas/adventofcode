34edf447258ebd903ca4b1943b5b635826c33f0f
Dvermaas <31657287+dvermaas@users.noreply.github.com>
2024-11-19 23:15:03 +0100

    day 3 complete

--- 2023/day3.py
+++ 2023/day3.py
@@ -1,18 +1,15 @@
-data = open("day3t.txt").read().splitlines()
+data = open("day3.txt").read().splitlines()
 
 
 def is_special_char_around(y, sx, x):
-    print("Checking:", data[y][sx:x])
     for row in data[max(0, y-1):y+2]:
-        print("row", row[max(0, sx-1):x+1])
         for item in row[max(0, sx-1):x+1]:
             if item != '.' and not item.isdigit():
-                # return True
-                pass
+                return True
     return False
 
 
-def find_the_numbers():
+def calculate_part_sum():
     out = 0
     for y, row in enumerate(data):
         x = 0
@@ -21,12 +18,10 @@ def find_the_numbers():
                 sx = x
                 while x < len(row) and row[x].isdigit():
                     x += 1
-                print("hmmm", (sx, x))
                 if is_special_char_around(y, sx, x):
                     out += int(row[sx:x])
             x += 1
     return out
 
 
-# 564267 is too high?
-print(f"Part 1: {find_the_numbers()}")
+print(f"Part 1: {calculate_part_sum()}")
