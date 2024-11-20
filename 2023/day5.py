4e30418c2403ddfb7e71303931f4503479096f88
Dvermaas <31657287+dvermaas@users.noreply.github.com>
2024-11-20 11:26:26 +0100

    day 5, destroying 32 gb of ram

--- 2023/day5.py
+++ 2023/day5.py
@@ -1,7 +1,7 @@
-data = open("day5t.txt").read().splitlines()
+data = open("day5.txt").read().splitlines()
 
 
-def parser():
+def parser() -> list:
     out = []
     sub_out = []
     for line in data:
@@ -11,12 +11,42 @@ def parser():
             out.append(sub_out)
             sub_out = []
     out[0] = out[0][0].split(": ")
-    print(out[0][1].split(" "))
     out = [
         [list(map(int, line.split(" "))) for line in group[1:]]
         for group in out
     ]
+    out[0] = out[0][0]
     return out
 
 
-print(parser())
+parsed_data = parser()
+
+
+def data_hasher(data_index: int) -> dict:
+    hashmap = {}
+    for d_range, s_range, length in parsed_data[data_index]:
+        for i in range(length):
+            hashmap[s_range + i] = d_range + i
+    return hashmap
+
+
+hash_dict = [data_hasher(i) for i in range(1, len(parsed_data))]
+
+
+def data_farmer(seed: int) -> int:
+    out = seed
+    for i in range(len(hash_dict)):
+        if out in hash_dict[i]:
+            out = hash_dict[i][out]
+    return out
+    
+
+def calculate_lowest_location():
+    return min([data_farmer(seed) for seed in parsed_data[0]])
+    
+    
+print(f"Part 1: {calculate_lowest_location()}")
+# print(data_farmer(79))
+# print(data_farmer(14))
+# print(data_farmer(55))
+# print(data_farmer(13))
