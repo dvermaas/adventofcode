68bd936229ff6386ed40fac25cda75f0f0cffff8
Dvermaas <31657287+dvermaas@users.noreply.github.com>
2024-11-20 19:37:22 +0100

    5.2 slow

--- 2023/day5.py
+++ 2023/day5.py
@@ -1,31 +1,23 @@
 from typing import Tuple
+from tqdm import tqdm
 
-data = open("data/day5.txt").read().splitlines()
+data = open("data/day5.txt").read().split("\n\n")
 
 
 def parser() -> Tuple[list, list]:
-    out = []
-    sub_out = []
-    for line in data:
-        if len(line) > 0:
-            sub_out.append(line)
-        else:
-            out.append(sub_out)
-            sub_out = []
-    out.append(sub_out)
-    out[0] = out[0][0].split(": ")
-    out = [
-        [list(map(int, line.split(" "))) for line in group[1:]]
-        for group in out
-    ]
-    return out[0][0], out[1:]
+    seed_list = list(map(int, data[0].split(": ")[1].split()))
+    conversion_data = []
+    for block in data[1:]:
+        sub_table = []
+        for line in block.splitlines()[1:]:
+            sub_table.append(list(map(int, line.split())))
+        conversion_data.append(sub_table)
+    return seed_list, conversion_data
 
 
 seeds, parsed_data = parser()
-for row in parsed_data:
-    print(row)
     
-    
+
 def fast_converter(data_index: int, seed: int) -> int:
     for d_range, s_range, length in parsed_data[data_index]:
         if seed in range(s_range, s_range + length):
@@ -33,21 +25,26 @@ def fast_converter(data_index: int, seed: int) -> int:
     return seed
 
 
-print(fast_converter(0, 79))
-
-
 def calculate_location(seed: int) -> int:
-    debug = [seed]
     out = seed
     for i in range(len(parsed_data)):
         out = fast_converter(i, out)
-        debug.append(out)
-    print('debug', debug)
     return out
         
 
-def calculate_lowest_location():
+def calculate_lowest_location() -> int:
     return min([calculate_location(seed) for seed in seeds])
-    
+
+
+def calculate_lowest_range_location() -> int:
+    out = calculate_location(seeds[0])
+    for i in range(0, len(seeds), 2):
+        for seed in tqdm(range(seeds[i], seeds[i] + seeds[i + 1])):
+            location = calculate_location(seed)
+            if location < out:
+                out = location
+    return out
+
     
 print(f"Part 1: {calculate_lowest_location()}")
+print(f"Part 2: {calculate_lowest_range_location()}")
