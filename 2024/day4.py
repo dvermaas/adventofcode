2f9ff0a91a697ec9e4ec062792a5de127ef6f605
Dvermaas <31657287+dvermaas@users.noreply.github.com>
2024-12-04 18:44:07 +0100

    day 4 part 2

--- 2024/day4.py
+++ 2024/day4.py
@@ -4,12 +4,12 @@ from aocd import get_data
 data = get_data(day=4, year=2024).splitlines()
 
 
-def part_1(puzzle_data: List[str], substring: str = "XMAS"):
-    out = []
+def part_1(puzzle_data: List[str], substring: str = "XMAS") -> int:
+    out = 0
     window = len(substring)
     for y, row in enumerate(puzzle_data):
         for x, item in enumerate(row):
-            out.append(sum([
+            out += sum([
                 row[x: x + window] == substring if x + window <= len(row) else False,
                 row[x: x + window] == substring[::-1] if x + window <= len(row) else False,
                 ''.join([puzzle_data[y+i][x] for i in range(window)]) == substring if y + window <= len(puzzle_data) else False,
@@ -18,8 +18,24 @@ def part_1(puzzle_data: List[str], substring: str = "XMAS"):
                 ''.join([puzzle_data[y+i][x+i] for i in range(window)]) == substring[::-1] if y + window <= len(puzzle_data) and x + window <= len(row) else False,
                 ''.join([puzzle_data[y - i][x + i] for i in range(window)]) == substring if y - window + 1 >= 0 and x + window <= len(row) else False,
                 ''.join([puzzle_data[y - i][x + i] for i in range(window)]) == substring[::-1] if y - window + 1 >= 0 and x + window <= len(row) else False,
-            ]))
-    return sum(out)
+            ])
+    return out
+
+
+def part_2(puzzle_data: List[str]):
+    out = 0
+    for y in range(1, len(puzzle_data)-1):
+        for x in range(1, len(puzzle_data[0])-1):
+            if not puzzle_data[y][x] == "A":
+                continue
+
+            d1, d2 = puzzle_data[y+1][x-1], puzzle_data[y-1][x+1]
+            d3, d4 = puzzle_data[y+1][x+1], puzzle_data[y-1][x-1]
+            if {d1, d2} != {"M", "S"} or {d3, d4} != {"M", "S"}:
+                continue
+            out += 1
+    return out
 
 
 print(f"Part 1: {part_1(data)}")
+print(f"Part 2: {part_2(data)}")
