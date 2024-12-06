e31f23364cafdab7538f82c8bb077688b9177aa1
dverm <dvermaas@live.com>
2024-12-06 12:18:16 +0100

    day 6 recursion

--- 2024/day6.py
+++ 2024/day6.py
@@ -1,10 +1,8 @@
 from typing import List, Tuple
 
 from aocd import get_data
-from collections import defaultdict
 
-data = [list(line) for line in open("test.txt").read().splitlines()]
-# data = get_data(day=5, year=2024)
+data = [list(line) for line in open("day6.txt").read().splitlines()]
 
 
 def find_guard(puzzle_input: List[List[str]]) -> Tuple[int, int]:
@@ -21,6 +19,9 @@ def print_puzzle(puzzle_input: List[List[str]]):
 
 
 def move_guard(puzzle_input: List[List[str]], guard_location: Tuple[int, int]):
+    print("debug", part_1(puzzle_input))
+    if part_1(puzzle_input) == 962:
+        print_puzzle(puzzle_input)
     guard = ("^", ">", "v", "<")
     offsets = ((-1, 0), (0, 1), (1, 0), (0, -1))
     y, x = guard_location
@@ -29,14 +30,21 @@ def move_guard(puzzle_input: List[List[str]], guard_location: Tuple[int, int]):
     if not 0 <= y+dy < len(puzzle_input) or not 0 <= x+dx < len(puzzle_input[0]):
         return puzzle_input
     if puzzle_input[y+dy][x+dx] == "#":
-        puzzle_input[y][x] = guard[index + 1 % 4]
-        return guard_location, puzzle_input
+        puzzle_input[y][x] = guard[(index + 1) % 4]
+        return move_guard(puzzle_input, guard_location)
     puzzle_input[y+dy][x+dx] = puzzle_input[y][x]
     puzzle_input[y][x] = "X"
     return move_guard(puzzle_input, (y+dy, x+dx))
 
 
-print_puzzle(data)
-for i in range(2):
-    move_guard(data, find_guard(data))
-print_puzzle(data)
+def part_1(puzzle_input: List[List[str]]) -> int:
+    out = 0
+    for line in puzzle_input:
+        for item in line:
+            if item == "X":
+                out += 1
+    return out + 1
+
+
+print_puzzle(move_guard(data, find_guard(data)))
+print(part_1(move_guard(data, find_guard(data))))
