d2e9c8732e5d4143c2c93750b527f3f81c80a92a
Dvermaas <31657287+dvermaas@users.noreply.github.com>
2024-12-06 13:37:58 +0100

    day6.2

--- 2024/day6.py
+++ 2024/day6.py
@@ -1,8 +1,15 @@
+import copy
+import sys
+import time
 from typing import List, Tuple
 
 from aocd import get_data
 
-data = [list(line) for line in open("day6.txt").read().splitlines()]
+# data = [list(line) for line in open("day6.txt").read().splitlines()]
+data = [list(line) for line in get_data(day=6, year=2024).splitlines()]
+
+# the default recursion limit of 1000 will kill the move_guard() function before it is done
+sys.setrecursionlimit(len(data) * len(data[0]))
 
 
 def find_guard(puzzle_input: List[List[str]]) -> Tuple[int, int]:
@@ -19,15 +26,13 @@ def print_puzzle(puzzle_input: List[List[str]]):
 
 
 def move_guard(puzzle_input: List[List[str]], guard_location: Tuple[int, int]):
-    print("debug", part_1(puzzle_input))
-    if part_1(puzzle_input) == 962:
-        print_puzzle(puzzle_input)
     guard = ("^", ">", "v", "<")
     offsets = ((-1, 0), (0, 1), (1, 0), (0, -1))
     y, x = guard_location
     index = guard.index(puzzle_input[y][x])
     dy, dx = offsets[index]
     if not 0 <= y+dy < len(puzzle_input) or not 0 <= x+dx < len(puzzle_input[0]):
+        puzzle_input[y][x] = "X"
         return puzzle_input
     if puzzle_input[y+dy][x+dx] == "#":
         puzzle_input[y][x] = guard[(index + 1) % 4]
@@ -46,5 +51,25 @@ def part_1(puzzle_input: List[List[str]]) -> int:
     return out + 1
 
 
-print_puzzle(move_guard(data, find_guard(data)))
-print(part_1(move_guard(data, find_guard(data))))
+def part_2(puzzle_input: List[List[str]]) -> int:
+    out = 0
+    guard_location = find_guard(puzzle_input)
+    for y, line in enumerate(puzzle_input):
+        for x, item in enumerate(line):
+            if item == "#" or (y, x) == guard_location:
+                continue
+            puzzle_input[y][x] = "#"
+            tmp = copy.deepcopy(puzzle_input)
+            try:
+                move_guard(tmp, guard_location)
+            except RecursionError:
+                out += 1
+            puzzle_input[y][x] = "."
+    return out
+
+
+solved_puzzle = move_guard(copy.deepcopy(data), find_guard(data))
+print("part 1:", part_1(solved_puzzle))
+s = time.time()
+print("part 2:", part_2(data))
+print("elapsed time:", time.time() - s)
