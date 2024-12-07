0d6556965feed909414ebf9882457018bb982508
Dvermaas <31657287+dvermaas@users.noreply.github.com>
2024-12-07 16:41:50 +0100

    fix naming and typing

--- 2024/day7.py
+++ 2024/day7.py
@@ -7,7 +7,7 @@ from aocd import get_data
 data = get_data(day=7, year=2024).splitlines()
 
 
-def lr_eval(result: int, numbers: List[int], operators: List[str]):
+def is_buildable(result: int, numbers: List[int], operators: List[str]) -> bool:
     out = numbers[0]
     for i, operator in enumerate(operators):
         if operator == "+":
@@ -19,14 +19,14 @@ def lr_eval(result: int, numbers: List[int], operators: List[str]):
     return out == result
 
 
-def part_1(puzzle_data: List[str], chars: tuple = ("+", "*")):
+def part_1(puzzle_data: List[str], chars: tuple = ("+", "*")) -> int:
     out = 0
     for line in tqdm(puzzle_data):
         result, numbers = line.split(": ")
         result, numbers = int(result), list(map(int, numbers.split(" ")))
         operators_list = list(product(chars, repeat=len(numbers)-1))
         for operators in operators_list:
-            if lr_eval(result, numbers, operators):
+            if is_buildable(result, numbers, operators):
                 out += result
                 break
     return out
